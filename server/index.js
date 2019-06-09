93db170e168f1a375427900de9fb1b6c6d07ab8b
salwaMugh <Sam8O.o@hotmail.com>
2019-06-09 11:19:38 +0300

    connect react-app & server relate to #38

--- server/index.js
+++ server/index.js
@@ -1,17 +1,6 @@
 const express = require('express');
 
 const app = express();
-console.log("Hi");
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
 
 const port = 5000;
-console.log("Hi2");
 app.listen(port, () => console.log(`Server running on port ${port}`));
