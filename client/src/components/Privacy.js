3998989a1fda32f86d0f6a0a0be032a783f66773
salwaMugh <Sam8O.o@hotmail.com>
2019-06-11 17:06:53 +0300

    add axios module and made example for it

--- client/src/components/Privacy.js
+++ client/src/components/Privacy.js
@@ -1,13 +1,29 @@
 import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 
 class Privacy extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>Privacy</h1>
-      </Fragment>
-    );
+  state={
+    imageURL:''
   }
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      this.setState({ imageURL: response.data.message });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  render() {
+     const { imageURL } = this.state;
+     return (
+       <Fragment>
+         <img src={imageURL} />
+         <h1>Privacy</h1>
+       </Fragment>
+
+     );
+   }
 }
 
 export default Privacy;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
-
+const axios = require('axios')
 const app = express();
+const port = process.env.PORT || 5000;
 
-const port = 5000;
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+const getBreeds = () => {
+  try {
+    return axios.get('https://dog.ceo/api/breeds/list/all')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const countBreeds = async () => {
+  const breeds = getBreeds()
+    .then(response => {
+      if (response.data.message) {
+        console.log(
+          `Got ${Object.entries(response.data.message).length} breeds`
+        )
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+countBreeds()
