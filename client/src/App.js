34dac54726c7ecb645cf92ef3856879c2e69436c
salwaMugh <Sam8O.o@hotmail.com>
2019-06-11 13:23:42 +0300

    wrap react route in context relate to #36

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Privacy from './components/Privacy';
 import Resources from './components/Resources';
 import Journal from './components/Journal';
 import Error404 from './components/Error404';
+import { MyProvider } from './components/Context';
 import {
   LANDING_URL,
   LOGIN_URL,
@@ -28,19 +29,21 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
+      <MyProvider>
       <GlobalStyle />
       <Switch>
-        <Route exact path={LANDING_URL} component={Landing} />
-        <Route exact path={LOGIN_URL} component={Login} />
-        <Route exact path={SIGNUP_URL} component={SignUp} />
-        <Route exact path={INFO_URL} component={Info} />
-        <Route exact path={DASHBOARD_URL} component={Dashboard} />
-        <Route exact path={CONTACT_URL} component={Contact} />
-        <Route exact path={PRIVACY_URL} component={Privacy} />
-        <Route exact path={RESOURCES_URL} component={Resources} />
-        <Route exact path={JOURNAL_URL} component={Journal} />
-        <Redirect to={ERROR404_URL} component={Error404} />
-      </Switch>
+          <Route exact path={LANDING_URL} component={Landing} />
+          <Route exact path={LOGIN_URL} component={Login} />
+          <Route exact path={SIGNUP_URL} component={SignUp} />
+          <Route exact path={INFO_URL} component={Info} />
+          <Route exact path={DASHBOARD_URL} component={Dashboard} />
+          <Route exact path={CONTACT_URL} component={Contact} />
+          <Route exact path={PRIVACY_URL} component={Privacy} />
+          <Route exact path={RESOURCES_URL} component={Resources} />
+          <Route exact path={JOURNAL_URL} component={Journal} />
+          <Redirect to={ERROR404_URL} component={Error404} />
+        </Switch>
+    </MyProvider>
     </BrowserRouter>
   );
 };
