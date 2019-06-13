559f182413e50d8d7a07370ac6235faacece7d58
Kristina Jaggard <jaggardk@gmail.com>
2019-06-13 14:39:42 +0100

    create a logo logout controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 const { signUp } = require('./signUp');
 const { login } = require('./login');
+const { logout } = require('./logout');
 
 module.exports = {
   signUp,
   login,
+  logout,
 };
--- server/router.js
+++ server/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signUp, login } = require('./controllers');
+const { signUp, login, logout } = require('./controllers');
 
 const router = express.Router();
 
-router.post('/signUp', signUp);
-router.post('/login', login);
+router.post('/account/signUp', signUp);
+router.post('/account/login', login);
+router.get('/account/logout', logout);
 
 module.exports = router;
