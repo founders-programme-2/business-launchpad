f888522a4dcd961fcdbce955619589e1fa78a070
salwaMugh <Sam8O.o@hotmail.com>
2019-06-13 17:14:57 +0300

    fix travis relate to #66

--- client/src/components/CHeader.js
+++ client/src/components/CHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Logo, HeaderWrapper, Line } from './CHeader.style.js';
 import logo from'../styles/logoYellow.svg';
