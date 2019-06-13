d03bfe599471f2b947a08db5e7036ec96eeb6c3c
Kristina Jaggard <jaggardk@gmail.com>
2019-06-13 14:54:19 +0100

    verify sessions token

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 const { signUp } = require('./signUp');
 const { login } = require('./login');
 const { logout } = require('./logout');
+const { verifySession } = require('./verifySession');
 
 module.exports = {
   signUp,
   login,
   logout,
+  verifySession,
 };
--- server/router.js
+++ server/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signUp, login, logout } = require('./controllers');
+const { signUp, login, logout, verifySession } = require('./controllers');
 
 const router = express.Router();
 
 router.post('/account/signUp', signUp);
 router.post('/account/login', login);
 router.get('/account/logout', logout);
+router.get('/account/verifySession', verifySession);
 
 module.exports = router;
