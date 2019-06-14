0df6653968f0b17359eb857b3ae8246128a29a70
Kristina Jaggard <jaggardk@gmail.com>
2019-06-14 18:18:18 +0100

    create a JWT token

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -3,3 +3,24 @@ const { ExtractJwt } = require('passport-jwt');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
+require('dotenv').config();
+
+const secret = process.env.secretOrKey;
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = secret;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) return done(null, user);
+          return done(null, false);
+        })
+        // eslint-disable-next-line no-console
+        .catch(err => console.log(`You have a JWT passport error ${err}`));
+    })
+  );
+};
