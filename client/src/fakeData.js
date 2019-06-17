3a41372ecdb8c7af4b5bfefdf10c05e8004c6171
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-17 19:32:36 +0100

    Create fake data file for testing front end of goal section

---
+++ client/src/fakeData.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'First goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+  {
+    id: 2,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'Second goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+  {
+    id: 3,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'Third goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+  {
+    id: 4,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'Fourth goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+  {
+    id: 5,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'Fifth goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+  {
+    id: 6,
+    'date-created': '2019-05-28',
+    checked: false,
+    title: 'Sixth goal',
+    category: 'Goal',
+    date: '2019-06-20',
+    details:
+      'Here is some pretty standard random text for the details section.',
+  },
+];
