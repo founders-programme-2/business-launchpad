d993cbc6f9c62e6a799aef059485d3343d76d400
Kristina Jaggard <jaggardk@gmail.com>
2019-06-17 17:13:41 +0100

    basic journal structure

--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -1,10 +1,34 @@
 import React, { Component, Fragment } from 'react';
+import * as S from './Journal.style';
 
 class Journal extends Component {
+  state = {
+    someState: '',
+  };
+
   render() {
     return (
       <Fragment>
-        <h1>Journal</h1>
+        <S.H1>
+          Business <br /> Journal
+        </S.H1>
+        <S.P>
+          Your journal is a place to log your goals, action points, mentor notes
+          and reflections.
+        </S.P>
+        <S.P>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{' '}
+        </S.P>
+        <S.Section>
+          <S.H2>Action Plan:</S.H2>
+          <S.P>
+            As you complete your goals, check them off and watch your business
+            grow!
+          </S.P>
+        </S.Section>
       </Fragment>
     );
   }
