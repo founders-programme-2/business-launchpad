20f8d6be3c6a37f50e26a98546954f89da3bab42
salwaMugh <Sam8O.o@hotmail.com>
2019-06-17 12:01:43 +0300

    fix travis

--- client/src/components/CMenu.js
+++ client/src/components/CMenu.js
@@ -1,3 +1,4 @@
+/*eslint no-unused-vars: "error"*/
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,12 +1,10 @@
 import React, { Component, Fragment } from 'react';
-import CMenu from './CMenu';
 import CHeader from './CHeader';
 
 class Contact extends Component {
   render() {
     return (
       <Fragment>
-
       <CHeader menu/>
       </Fragment>
     );
