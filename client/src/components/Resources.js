b1db8af23d14479ce8a21e36776f2345f38bf35b
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-17 21:23:18 +0100

    Creat very basic layout for resources page

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -1,10 +1,30 @@
 import React, { Component, Fragment } from 'react';
+import * as S from './Resources.style';
 
 class Resources extends Component {
+  state = {
+    funding: [],
+    events: [],
+    businessDevelopment: [],
+    books: [],
+  };
+
   render() {
     return (
       <Fragment>
-        <h1>Resources</h1>
+        <S.H1>Resources</S.H1>
+        <S.Section>
+          <S.H2>Funding</S.H2>
+        </S.Section>
+        <S.Section>
+          <S.H2>Events</S.H2>
+        </S.Section>
+        <S.Section>
+          <S.H2>Business Development</S.H2>
+        </S.Section>
+        <S.Section>
+          <S.H2>Books Worth Reading</S.H2>
+        </S.Section>
       </Fragment>
     );
   }
