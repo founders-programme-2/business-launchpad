6ca42e82fda80e5707aa9d4d07fc8497535212e5
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-17 21:56:37 +0100

    Create basic airtable call and connect to route

--- server/router.js
+++ server/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, logout } = require('./controllers');
+const { register, login, logout, getResources } = require('./controllers');
 
 const router = express.Router();
 
 router.post('/account/register', register);
 router.post('/account/login', login);
 router.get('/account/logout', logout);
+router.get('/resources/get', getResources);
 
 module.exports = router;
