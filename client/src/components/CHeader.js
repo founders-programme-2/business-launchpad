b5b21d576cc00f814b8991ff98037be3457b7467
salwaMugh <Sam8O.o@hotmail.com>
2019-06-17 11:42:40 +0300

    make the header dynamic according to pages relate to #66

--- client/src/components/CHeader.js
+++ client/src/components/CHeader.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
 import { Header, Logo, HeaderWrapper, Line } from './CHeader.style';
 import logo from '../assets/logoWhite.svg';
 import info from '../assets/info.svg';
+import { CMenu } from './CMenu';
 import { LANDING_URL, INFO_URL } from '../constants';
 
-const CHeader = props => (
-  <Header>
-    <HeaderWrapper>
-      <Link to={LANDING_URL}>
-        <Logo src={logo} alt="Launchpad logo" />
-      </Link>
-      <Link to={INFO_URL}>
-        <Logo src={info} alt="Launchpad logo" primary />
-      </Link>
-    </HeaderWrapper>
-    <Line />
-  </Header>
-);
+class CHeader extends Component {
+  state = {
+
+  };
+
+  render() {
+const { menu } = this.props;
+    return (
+      <Header>
+        <HeaderWrapper>
+          <Link to={LANDING_URL}>
+            <Logo src={logo} alt="Launchpad logo" />
+          </Link>
+          {menu ? (
+            <CMenu />
+          ) : (
+            <Link to={INFO_URL}>
+              <Logo src={info} alt="Launchpad logo" primary />
+            </Link>
+          )}
+        </HeaderWrapper>
+        <Line />
+      </Header>
+    );}
+
+};
 export default CHeader;
