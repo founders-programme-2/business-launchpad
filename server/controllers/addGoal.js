367701ccdd03b0a7ab47ce3b380251d5674fcb71
Kristina Jaggard <jaggardk@gmail.com>
2019-06-18 16:16:02 +0100

    backend add goal complete

--- server/controllers/addGoal.js
+++ server/controllers/addGoal.js
@@ -1,1 +1,27 @@
-console.log('ITS FUCKEN WORKEN');
+const User = require('../database/models/User');
+
+const goal = {
+  title: 'Test title',
+};
+
+const addGoal = (req, res) => {
+  const { _id } = req.body;
+
+  User.findOne({ _id }).then(foundUser => {
+    if (!foundUser) {
+      return res.status(400).send({
+        success: false,
+        message: 'User not found',
+      });
+    }
+    foundUser.goals.push(goal);
+    foundUser.save(err => {
+      if (err) {
+        console.log(`Sorry you've had an error saving a goal! Error:${err}`);
+      }
+      console.log(`Yay!`);
+    });
+  });
+};
+
+module.exports = { addGoal };
--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const goalSchema = require('./Goal');
 
+
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
