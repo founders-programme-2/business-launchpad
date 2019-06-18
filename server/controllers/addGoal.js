6e5145bb063902292f18b5eeab9d18c5ad10545e
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-18 15:30:02 +0100

    create and connect empty controller

---
+++ server/controllers/addGoal.js
@@ -0,0 +1,1 @@
+console.log('ITS FUCKEN WORKEN');
--- server/router.js
+++ server/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, logout } = require('./controllers');
+const { register, login, logout, addGoal } = require('./controllers');
 
 const router = express.Router();
 
 router.post('/account/register', register);
 router.post('/account/login', login);
 router.get('/account/logout', logout);
+router.post('/account/goal', addGoal);
 
 module.exports = router;
