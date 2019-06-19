f1ce1bfaf779e655eff2d0b4b71861e307bb84b4
shaima96 <141062@ppu.edu.ps>
2019-06-19 14:51:46 -0500

    Create landing page #56

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,16 @@
 import React, { Component, Fragment } from 'react';
+import * as S from './CBtn.style';
+import logo from '../assets/MY.png';
 
 class Landing extends Component {
   render() {
     return (
       <Fragment>
-        <h1>This is a h1</h1>
+        <S.Main>
+          <S.H1> Business launchpad </S.H1>
+          <S.Button onClick={this.login}>login </S.Button>
+          <S.Button onClick={this.signup}>signup </S.Button>
+        </S.Main>
       </Fragment>
     );
   }
