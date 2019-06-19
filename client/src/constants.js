3a973b6d0b7fd4e02b3efe6ec6931967c00b7060
shaima96 <141062@ppu.edu.ps>
2019-06-19 10:20:57 -0500

    update in path for login and signup

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 export const LANDING_URL = '/';
-export const LOGIN_URL = '/login';
-export const SIGNUP_URL = '/sign-up';
+export const LOGIN_URL = '/account/login';
+export const SIGNUP_URL = '/account/sign-up';
 export const INFO_URL = '/info';
 export const DASHBOARD_URL = '/dashboard';
 export const CONTACT_URL = '/contact';
