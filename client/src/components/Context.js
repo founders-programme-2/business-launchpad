4c3886c9ba793225ee690e78c9de07396c45b28b
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-19 17:04:16 +0100

    Export MyContext from context and add static placeholder id

--- client/src/components/Context.js
+++ client/src/components/Context.js
@@ -1,13 +1,16 @@
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyContext = React.createContext();
+export const MyContext = React.createContext();
 export class MyProvider extends Component {
   state = {
     isLoggedIn: false,
     token: '',
+    // for development this should be a static id of a document in your database
+    // until login/authentication automatically starts to save it here
+    _id: '5d0912f508d0f83945e080dd',
   };
-
 
   render() {
     const { children } = this.props;
