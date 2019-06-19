07cc721db426780b744316063fac763e10734b72
shaima96 <141062@ppu.edu.ps>
2019-06-19 15:03:46 -0500

    create css style for CBtn component #68

--- client/src/components/CBtn.style.js
+++ client/src/components/CBtn.style.js
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const Button = style.Button``;
+const Button = styled.Button`
+  width: 168px;
+  height: 32px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  background-color: var(--black);
+  color: var(--login-color, white);
+  font-weight: 700;
+  padding: 0.1rem;
+  font-size: 1.5rem;
+  letter-spacing: 8%;
+`;
+
+export default Button;
