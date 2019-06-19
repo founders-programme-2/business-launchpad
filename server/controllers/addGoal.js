50c1efba8d2f788bca7021ccaae64b21dcb4a5e2
Kristina Jaggard <jaggardk@gmail.com>
2019-06-19 09:43:44 +0200

    refactor addGoal

--- server/controllers/addGoal.js
+++ server/controllers/addGoal.js
@@ -1,10 +1,6 @@
 const User = require('../database/models/User');
 const { validateGoal } = require('../middleware/validation/validateGoal');
 
-const goal = {
-  title: 'Test title2',
-};
-
 const addGoal = (req, res) => {
   const { errors, isValid } = validateGoal(req.body);
   if (!isValid) {
@@ -20,7 +16,7 @@ const addGoal = (req, res) => {
         message: 'User not found',
       });
     }
-    foundUser.goals.push(goal);
+    foundUser.goals.push(req.body);
     foundUser.save(err => {
       if (err) {
         console.log(`Sorry you've had an error saving a goal! Error:${err}`);
