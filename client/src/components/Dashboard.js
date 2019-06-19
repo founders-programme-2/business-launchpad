4ccabbb45f429f84e0296c5e83eee2c292ccd92a
salwaMugh <Sam8O.o@hotmail.com>
2019-06-19 21:50:46 +0300

    creating dashboard page relate to #60

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React, { Component, Fragment } from 'react';
+import GoalProgress from './Dashboard-ProgressGoal.js'
+import * as C from './Journal.style';
 
 class Dashboard extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Dashboard</h1>
+        <C.H1>
+          hello <br/>
+        jane doe
+      </C.H1>
+<GoalProgress/>
       </Fragment>
     );
   }
