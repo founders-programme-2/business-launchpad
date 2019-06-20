c4d436c04c35279b88eb703c36420c164ed98b76
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-20 10:18:58 +0100

    Create basic database call to retrieve all goal records with an id

--- server/controllers/getGoals.js
+++ server/controllers/getGoals.js
@@ -1,6 +1,21 @@
+const User = require('../database/models/User');
+
 const getGoals = (req, res) => {
-  // this is the function to retrieve all goals
-  console.log('get goals path is working');
+  const { _id } = req.body;
+
+  return User.findById(_id, 'goals')
+    .then(goals => {
+      res.status(200).send({
+        success: true,
+        data: goals,
+      });
+    })
+    .catch(err => {
+      res.send({
+        success: false,
+        error: err,
+      });
+    });
 };
 
 module.exports = { getGoals };
