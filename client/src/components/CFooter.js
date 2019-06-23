e2177569999a5f812641041911e161cf460f7723
shaima96 <141062@ppu.edu.ps>
2019-06-23 11:05:25 -0500

    Create footer component #67

---
+++ client/src/components/CFooter.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import * as S from './Login.style';
+import CHeader from './CHeader';
+import { CONTACT_URL, PRIVACY_URL } from '../constants';
+
+class CFooter extends Component {
+  render() {
+    return (
+      );
+  }
+}
+
+CFooter.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
+
+export default CFooter;
