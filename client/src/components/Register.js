47bd2e891f401d99864d6a6dca8c4aec4cfc7be6
Kristina Jaggard <jaggardk@gmail.com>
2019-06-24 14:57:53 +0200

    remove unneeded code

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -17,14 +17,14 @@ class Register extends Component {
     errors: {},
   };
 
-  componentDidMount() {
-    const { auth, history } = this.props;
-    const { isAuthenticated } = auth;
-    // If logged in and user navigates to Register page, should redirect them to dashboard
-    if (isAuthenticated) {
-      history.push('/dashboard');
-    }
-  }
+  // componentDidMount = () => {
+  //   const { auth, history } = this.props;
+  //   const { isAuthenticated } = auth;
+  //   // If logged in and user navigates to Register page, should redirect them to dashboard
+  //   if (isAuthenticated) {
+  //     history.push('/dashboard');
+  //   }
+  // };
 
   componentWillReceiveProps = nextProps => {
     if (nextProps.errors) {
@@ -40,7 +40,7 @@ class Register extends Component {
 
   // Register user
   onSubmit = e => {
-    const { history } = this.props;
+    const { history, registerUser } = this.props;
     const { name, email, password, password2 } = this.state;
     e.preventDefault();
 
@@ -109,20 +109,6 @@ class Register extends Component {
   }
 }
 
-// mapStateToProps allows us to get our state from Redux and map it to props which we can use inside components.
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors,
-});
-
-// connect() - connects our components to our Redux store
-// withRouter(Register) allow us to redirect within an action use react router dom
-
-export default connect(
-  mapStateToProps,
-  { registerUser }
-)(withRouter(Register));
-
 Register.propTypes = {
   history: PropTypes.object.isRequired,
   registerUser: PropTypes.func.isRequired,
@@ -130,198 +116,19 @@ Register.propTypes = {
   errors: PropTypes.object.isRequired,
 };
 
-// class Register extends Component {
-//   state = {
-//     name: '',
-//     email: '',
-//     password: '',
-//     confirmPassword: '',
-//     nameError: '',
-//     emailError: '',
-//     passwordError: '',
-//   };
+// mapStateToProps allows us to get our state from Redux and map it to props which we can use inside components.
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+});
 
-//   // componentDidMount() {
-//   //   axios.get('/checkCookie').then(({ data: { cookie } }) => {
-//   //     if (cookie) {
-//   //       const { history } = this.props;
-//   //       history.push(LOGIN_URL);
-//   //     }
-//   //   });
-//   // }
+// connect() - connects our components to our Redux store
+// mapStateToProps allows us to 'capture' the data from our state and map it to props that we can use
+// { registerUser } is the props we want to pull from the store
+// withRouter(Register) allow us to redirect within an action use react router dom
 
-//   // validate = () => {
-//   //   const {
-//   //     name,
-//   //     password,
-//   //     confirmPassword,
-//   //     nameError,
-//   //     emailError,
-//   //     passwordError,
-//   //   } = this.state;
-//   //   let err = false;
-//   //   // List of characters to avoid in name
-//   //   const characters = [
-//   //     '+',
-//   //     '/',
-//   //     '*',
-//   //     '$',
-//   //     '#',
-//   //     '@',
-//   //     '!',
-//   //     '^',
-//   //     '&',
-//   //     '(',
-//   //     ')',
-//   //     '?',
-//   //     '>',
-//   //     '<',
-//   //     '.',
-//   //   ];
-//   //   for (let i = 0; i < characters.length; i + 1) {
-//   //     if (name.includes(characters[i])) {
-//   //       this.setState({
-//   //         nameError:
-//   //           'Name should only contain letters, numbers, underscores and dashes.',
-//   //       });
-//   //       err = true;
-//   //     } else if (name < 1) {
-//   //       this.setState({
-//   //         nameError: 'Please provide a name.',
-//   //       });
-//   //       err = true;
-//   //     }
-//   //     if (password !== confirmPassword) {
-//   //       this.setState({
-//   //         passwordError: 'Passwords do not match.',
-//   //       });
-//   //       err = true;
-//   //     } else if (password.length < 6 && password.length > 30) {
-//   //       this.setState({
-//   //         passwordError: 'Password needs to be between 6 to 30 characters.',
-//   //       });
-//   //       err = true;
-//   //     }
-//   //   }
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
 
-//   //   if (nameError || passwordError || emailError) {
-//   //     // eslint-disable-next-line no-alert
-//   //     alert(`Error: ${nameError || passwordError || emailError}`);
-//   //   }
-
-//   //   return err;
-//   // };
-
-//   register = () => {
-//     //const err = this.validate();
-//     // if (!err) {
-//       const { history } = this.props;
-//       const { name, email, password, confirmPassword } = this.state;
-//       const inputs = {
-//         name,
-//         email,
-//         password,
-//         confirmPassword,
-//       };
-
-//       axios.post(REGISTER_URL, inputs).then(({ data }) => {
-//         if (data.success) {
-//           history.push({
-//             pathname: 'LOGIN_URL',
-//           });
-//         } else {
-//           this.setState({
-//             nameError: 'This name already exists.',
-//             isErrorName: true,
-//           });
-//         }
-//       });
-//     }
-//   // };
-
-//   render() {
-//     const {
-//       isErrorEmail,
-//       isErrorName,
-//       isErrorPassword,
-//       nameError,
-//       name,
-//       emailError,
-//       email,
-//       passwordError,
-//       password,
-//       confirmPassword,
-//     } = this.state;
-//     return (
-//       <Fragment>
-//         <CHeader />
-//         <S.Main>
-//           <S.H1>Sign up</S.H1>
-//           <form>
-//             <S.Label for="name"> Name: </S.Label>
-//             <S.Input
-//               StyleError={isErrorName}
-//               type="text"
-//               name="name"
-//               label="name"
-//               errorText={nameError}
-//               value={name}
-//               onChange={e =>
-//                 this.setState({
-//                   name: e.target.value,
-//                 })
-//               }
-//             />
-//             <S.Label for="email"> Email: </S.Label>
-//             <S.Input
-//               StyleError={isErrorEmail}
-//               type="text"
-//               name="email"
-//               label="email"
-//               errorText={emailError}
-//               value={email}
-//               onChange={e =>
-//                 this.setState({
-//                   email: e.target.value,
-//                 })
-//               }
-//             />
-//             <S.Label for="password"> Password: </S.Label>
-//             <S.Input
-//               StyleError={isErrorPassword}
-//               type="password"
-//               name="password"
-//               label="password"
-//               errorText={passwordError}
-//               value={password}
-//               onChange={e =>
-//                 this.setState({
-//                   password: e.target.value,
-//                 })
-//               }
-//             />
-//             <S.Label for="confirmPassword">Confirm Password</S.Label>
-//             <S.Input
-//               StyleError={isErrorPassword}
-//               type="password"
-//               name="confirmPassword"
-//               label="confirmPassword"
-//               errorText={passwordError}
-//               value={confirmPassword}
-//               onChange={e =>
-//                 this.setState({
-//                   confirmPassword: e.target.value,
-//                 })
-//               }
-//             />
-//           </form>
-//           <S.Button onClick={this.register}>Sign up</S.Button>
-//         </S.Main>
-//       </Fragment>
-//     );
-//   }
-// }
-
-// Register.propTypes = {
-//   history: ReactRouterPropTypes.history.isRequired,
-// };
