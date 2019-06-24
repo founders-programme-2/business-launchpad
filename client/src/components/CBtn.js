34e85bca78b7dddaaad38c407f59b56c599c3aca
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-24 19:31:03 +0100

    Add some basic prop types

--- client/src/components/CBtn.js
+++ client/src/components/CBtn.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './CBtn.style';
 
 const CBtn = props => {
@@ -8,6 +9,11 @@ const CBtn = props => {
       {text}
     </Button>
   );
+};
+
+CBtn.propTypes = {
+  text: PropTypes.string.isRequired,
+  cb: PropTypes.func.isRequired,
 };
 
 export default CBtn;
