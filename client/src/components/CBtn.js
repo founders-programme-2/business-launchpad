f996a8b5abf1b1cd1793e68455d4da9164f2129a
shaima96 <141062@ppu.edu.ps>
2019-06-25 13:13:11 -0500

    Button component

--- client/src/components/CBtn.js
+++ client/src/components/CBtn.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './CBtn.style';
 
-const CBtn = ({ text, path }) => (
-  <Button onClick={path} type="button">
-    {text}
-  </Button>
-);
+class CBtn extends Component {
+  state = {
+    email: '',
+    password: '',
+    text: '',
+    path: '',
+    confirmPassword: '',
+  };
+
+  login = () => {
+    const { email, password } = this.state;
+    const { history } = this.props;
+    const inputs = {
+      email,
+      password,
+    };
+  };
+
+  signup = () => {
+    const { email, password, confirmPassword } = this.state;
+    const { history } = this.props;
+    const inputs = {
+      email,
+      password,
+      confirmPassword,
+    };
+  };
+
+  render() {
+    const { text, path } = this.state;
+    return (
+      <Button onClick={path} type="button">
+        {text}
+      </Button>
+    );
+  }
+}
 
 export default CBtn;
