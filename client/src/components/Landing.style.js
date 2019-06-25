13b2ef58f834b012a311c9699f274625a9c36a9e
shaima96 <141062@ppu.edu.ps>
2019-06-25 12:08:44 -0500

    landing page

--- client/src/components/Landing.style.js
+++ client/src/components/Landing.style.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
   background-color: var(--highlight);
   height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   flex-flow: column wrap;
@@ -12,7 +13,6 @@ export const Main = styled.main`
 export const H1 = styled.h1`
   width: 146px;
   height: 60px;
-  top: 180px;
   font-family: Permanent Marker;
   font-weight: bold;
   font-size: 5rem;
@@ -21,21 +21,14 @@ export const H1 = styled.h1`
 export const H2 = styled.h2`
   font-style: var(--black);
   background-color: var(--white);
-  margin: 50px;
-  margin-bottom: 250px;
-  margin-left: 20px;
+  margin: 30px;
 `;
 export const Button = styled.button`
-  position: absolute;
   width: 243px;
   height: 46px;
-  left: 500px;
-  top: 500px;
   backgound: var(--white);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
-  margin-right: 40px;
-  margin-top: 20px;
   font-weight: bold;
   font-size: 1.5rem;
   letter-spacing: 0.08em;
@@ -43,20 +36,16 @@ export const Button = styled.button`
     background-color: var(--black);
     color: var(--white);
   }
+  margin: 20px;
 `;
 export const Button1 = styled.button`
-  position: absolute;
   width: 243px;
   height: 46px;
-  left: 420px;
-  top: 500px;
   background: var(--white);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
-  margin-top: 20px;
   font-weight: bold;
   font-size: 1.5rem;
-  margin: 80px;
   letter-spacing: 0.08em;
   &:hover {
     background-color: var(--black);
