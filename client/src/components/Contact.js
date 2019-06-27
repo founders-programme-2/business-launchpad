84ae346aa5d882d8f230ac58ca300cef8f8585be
shaima96 <141062@ppu.edu.ps>
2019-06-26 23:24:22 -0500

    contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,14 +1,40 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import * as S from './Contact.style';
 import CHeader from './CHeader';
+import CFooter from './CFooter';
 
-class Contact extends Component {
-  render() {
-    return (
-      <Fragment>
-        <CHeader menu />
-      </Fragment>
-    );
-  }
-}
+const Contact = () => {
+  return (
+    <Fragment>
+      <CHeader menu />
+      <S.Main>
+        <S.H1>contact</S.H1>
+        <S.ContactWrapper>
+          <S.H3>
+            <u>
+              business
+              <br />
+              launchpad
+            </u>
+          </S.H3>
+          <S.P>
+            Trident Business Centre <br /> 89 Bickersteth Rd,
+            <br /> Tooting, London <br /> SW17 9SH <br /> Phone:
+            <br /> 020 8516 7759
+            <br />
+            <br />
+            fakemail@fakeemail.com
+          </S.P>
+        </S.ContactWrapper>
+      </S.Main>
+      <CFooter />
+    </Fragment>
+  );
+};
+
+Contact.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
 
 export default Contact;
---
+++ client/src/components/Contact.style.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--white);
+  height: 100vh;
+  align-items: center;
+  flex-flow: column wrap;
+  justify-content: center;
+`;
+
+export const H1 = styled.h1`
+  font-weight: bold;
+  font-size: 3rem;
+  background-color: var(--highlight);
+  margin-bottom: 2%;
+`;
+export const H3 = styled.h3`
+  font-weight: bold;
+  font-size: 1rem;
+  color: var(--black);
+  margin-left: 29%;
+  margin-top: 3%;
+  background: var(--highlight);
+  width: 105px;
+`;
+export const ContactWrapper = styled.div`
+  background: #ffffff;
+  border: 3px solid #000000;
+  box-sizing: border-box;
+  width: 287px;
+  height: 200px;
+`;
+
+export const P = styled.p`
+  width: 80%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 18px;
+  letter-spacing: 0.1em;
+  margin-left: 10%;
+`;
