7be2b3ed22e537f00edcc62d372a0c4dae888e02
shaima96 <141062@ppu.edu.ps>
2019-06-28 17:13:48 -0500

    update info page

--- client/src/components/Info.style.js
+++ client/src/components/Info.style.js
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   display: flex;
+  flex-direction: column;
   background-color: var(--white);
-  height: 80vh;
   align-items: center;
-  flex-flow: column wrap;
-  justify-content: center;
+  flex-flow: column;
 `;
 
 export const H1 = styled.h1`
@@ -18,12 +17,13 @@ export const H1 = styled.h1`
   letter-spacing: 0.1em;
   font-family: 'Work Sans';
   font-weight: 900;
+  text-transform: uppercase;
 `;
 
 export const P = styled.p`
   width: 100%;
   font-family: 'Roboto';
-  margin-left: 5%;
   font-style: normal;
-  font-weight: normal;
+  font-weight: 900;
+  margin: 2rem;
 `;
