cde1e3be48a6e7d487215e4e1430fe3ead4cae5e
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-06-28 13:17:45 +0100

    Remove 400 status from error responses and include success field in successful response

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -8,16 +8,15 @@ const register = (req, res) => {
   // validate data sent in registration using validation functions
   const { errors, isValid } = validateRegister(req.body);
   const { name, email, password } = req.body;
-
   if (!isValid) {
-    return res.status(400).send(errors);
+    return res.send(errors);
   }
 
   // if the information sent in request is valid, search User table to see if user already exists
   return User.findOne({ email }).then(user => {
     if (user) {
-      return res.status(400).send({
-        success: true,
+      return res.send({
+        success: false,
         message: 'Sorry, that email already exists',
       });
     }
@@ -35,7 +34,12 @@ const register = (req, res) => {
         newUser.password = hash;
         newUser
           .save()
-          .then(registeredUser => res.send(registeredUser))
+          .then(registeredUser =>
+            res.send({
+              success: true,
+              data: registeredUser,
+            })
+          )
           .catch(error2 =>
             // eslint-disable-next-line no-console
             console.log(`You have a hashing error: ${error2}`)
