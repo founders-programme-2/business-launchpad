163fc4afd82bf14f9ceaac85e73d2d6fa3690611
shaima96 <141062@ppu.edu.ps>
2019-06-28 17:32:13 -0500

    update privacy page

--- client/src/components/Privacy.style.js
+++ client/src/components/Privacy.style.js
@@ -2,29 +2,29 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   display: flex;
-  flex-direction: column;
   background-color: var(--white);
-  height: 100vh;
   align-items: center;
-  flex-flow: column wrap;
+  flex-flow: column;
   justify-content: center;
 `;
 
 export const H1 = styled.h1`
-  font-weight: bold;
+  font-weight: 900;
   font-size: 3rem;
   background-color: var(--highlight);
   text-align: center;
-  margin-bottom: 3%;
-  margin-right: 13%;
+  text-transform: uppercase;
+  font-style: normal;
+  font-family: 'Work Sans';
+  justify-content: left;
 `;
 export const P = styled.p`
-  font-family: Roboto;
+  font-family: 'Roboto';
   font-style: normal;
   font-weight: normal;
   font-size: 1rem;
   line-height: 18px;
   letter-spacing: 0.1em;
-  width: 30%;
-  margin-bottom: 15%;
+  width: 100%;
+  margin: 2rem;
 `;
