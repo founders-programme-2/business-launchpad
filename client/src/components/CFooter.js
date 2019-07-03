06503dc370edeb925a6a24a317882218e194678b
shaima96 <141062@ppu.edu.ps>
2019-07-03 16:42:17 -0500

    fix footer section

--- client/src/components/CFooter.js
+++ client/src/components/CFooter.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './CFooter.style';
-import { CONTACT_URL, PRIVACY_URL } from '../constants';
+import { CONTACT_URL, PRIVACY_URL, LOGIN_URL } from '../constants';
 
-const CFooter = () => {
-  return (
-    <S.Footer>
-      <S.Line />
-      <S.FooterWrapper>
-        <Link to={PRIVACY_URL}>
-          <S.H3>Privacy Policy</S.H3>
-        </Link>
-        <Link to={CONTACT_URL}>
-          <S.H3>Contact Us </S.H3>
-        </Link>
-      </S.FooterWrapper>
-    </S.Footer>
-  );
+const CFooter = location => {
+  const { pathname } = location;
+  if (pathname !== LOGIN_URL) {
+    return (
+      <S.Footer>
+        <S.Line />
+        <S.FooterWrapper>
+          {pathname !== PRIVACY_URL && (
+            <Link to={PRIVACY_URL}>
+              <S.H3>Privacy Policy</S.H3>
+            </Link>
+          )}
+          {pathname !== CONTACT_URL && (
+            <Link to={CONTACT_URL}>
+              <S.H3>Contact Us </S.H3>
+            </Link>
+          )}
+        </S.FooterWrapper>
+      </S.Footer>
+    );
+  }
+  return null;
 };
 
 export default CFooter;
