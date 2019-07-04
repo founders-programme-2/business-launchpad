9005ca575b0851f3fb21e99de491242e6fd1fcf3
salwaMugh <Sam8O.o@hotmail.com>
2019-07-04 16:48:09 +0300

    handel login server error

--- server/controllers/serverError.js
+++ server/controllers/serverError.js
@@ -3,7 +3,4 @@ exports.serverError = (err, req, res, next) => {
   return res
     .status(500)
     .send({ success: false, message: 'Internal Server Error' });
-  // return res
-  //   .status(err.status || 500)
-  //   .send({ Error: err.message || 'Internal Server Error' });
 };
