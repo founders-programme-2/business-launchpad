403dde1a70c81b3fe04bfedb62b87c90e86ffeb1
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-07-04 20:56:12 +0200

    Change port to PORT in server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server running on port ${port}`));
